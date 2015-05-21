ad1198b70cae0326bdbebfe975d2f6fc9dd1dcf5
Stephen Parente <stephen@ingenuitydesign.com>
2015-05-21 13:44:36 -0400

    Added conditional comments for IE to hopefully solve preloading issue
    Also changed ff bash script to use current path

--- phantomjs/graphics/ff.sh
+++ phantomjs/graphics/ff.sh
@@ -11,14 +11,14 @@ for i in $( ls | grep Stat_ ); do
 	ffmpeg -i export/$i/animation.mp4 -c:v libtheora -c:a libvorbis export/$i/animation.ogv
 	ffmpeg -i export/$i/animation.mp4 -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis export/$i/animation.webm
 
-	rm ../../PGI/video/full/$i/fallback.jpg
-	cp $i/fallback.jpg ../../PGI/video/full/$i/fallback.jpg
+	rm ../PGI/video/full/$i/fallback.jpg
+	cp $i/fallback.jpg ../PGI/video/full/$i/fallback.jpg
 
 #	convert -loop 1 -delay 1/40 export/$i/*.png export/$i/animation.gif
 	# Copy all of these things
 	find "export/$i" -name 'animation.*' | while IFS= read -r foo; do 
 	   file_name=$(basename $foo)
-	   cp "$foo" "../../PGI/video/full/$i/$file_name"
+	   cp "$foo" "../../video/full/$i/$file_name"
 	done 
 	echo "Done converting $i"
 done
